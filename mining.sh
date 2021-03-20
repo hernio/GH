7c67f6b1f2da34873278d8b1eee933c0690d5aeb
hernio <33852317+hernio@users.noreply.github.com>
2021-03-20 10:35:49 +0700

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u Ms4V7VhX3XWY3V7DG5puFRGdtyXPQrJyxF.JOVANO -t1
